099d9e2e4093550f8c9de015d659d43003c0aeb8
[AgustinAmorena] <agustin.amorena@correo.ucu.edu.uy>
2021-09-26 19:02:24 -0300

    Test

--- src/test/Test.Library/UnitTest1.cs
+++ src/test/Test.Library/UnitTest1.cs
@@ -1,19 +1,37 @@
 using NUnit.Framework;
 using Library;
 
-namespace Test.Library
+namespace Test.Library;
+
+
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        public SpellsBook book = new SpellsBook();
+        public book.Spells = new Spell[]{ new Spell() };
+
+        public Wizard gandalf = new Wizard("Gandalf");
+        public gandalf.Staff = new Staff();
+        public gandalf.SpellsBook = book;
+
+        public Dwarf gimli = new Dwarf("Gimli");
+        public gimli.Axe = new Axe();
+        public gimli.Helmet = new Helmet();
+
+        [Test]
+        public void ReceiveAttackTest()
         {
+            int expected = gandalf.health - gimli.Axe.attack;
+            gandlf.ReceiveAttack(gimli.AttackValue());
+            int actual = gandalf.health;
+            Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void Test1()
+        public void CureTest()
         {
             Assert.Pass();
         }
+
     }
 }
